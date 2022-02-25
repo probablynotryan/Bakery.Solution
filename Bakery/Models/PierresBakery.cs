f974d466236a53b41ffcb809c96afb933d1ac402
Ryan Bass <theryanbass@gmail.com>
2022-02-25 09:59:22 -0800

    add pastry method for testing

--- Bakery/Models/PierresBakery.cs
+++ Bakery/Models/PierresBakery.cs
@@ -26,5 +26,43 @@ namespace Bakery.Model
       }
     return breadsCost;
    }
+   public int PastryOrder (int pastries)
+   {
+     int pastryCost = 0;
+     int countedPastries = pastries;
+     for (int i = 1; i <= pastries; i++)
+     {
+      if (countedPastries >= 6) 
+      {
+        pastryCost += 10;
+        countedPastries -= 6;
+      } 
+      else if (countedPastries == 5)
+      {
+        pastryCost += 9;
+        countedPastries -= 5;
+      }
+      else if (countedPastries == 4)
+      {
+        pastryCost += 7;
+        countedPastries -= 4;
+      }
+      else if (countedPastries == 3)
+      {
+        pastryCost += 5;
+        countedPastries -= 3;
+      }
+      else if (countedPastries >= 1)
+      {
+        pastryCost += 2;
+        countedPastries -= 1;
+      }
+      else 
+      {
+        continue;
+      }
+    }
+   return pastryCost;
+   }
   }
 }
