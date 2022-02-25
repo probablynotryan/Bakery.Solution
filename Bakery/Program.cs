e07cf11810c0faa7733e869bdb54b1c3ce45cb56
Ryan Bass <theryanbass@gmail.com>
2022-02-25 14:24:48 -0800

    add initial body of dialog

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -6,7 +6,16 @@ namespace Bakery
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierres' Bakery!");
+      string doesntMatter;
+      Console.WriteLine("Who's that? Whatcha want?");
+      Console.ReadLine(doesntMatter);
+      Console.WriteLine("You say bread? We got buy 2 get 1 today.")
+      Console.WriteLine("How many chu want? Before I change my mind.");
+      Console.ReadLine(aStringVar);
+      Console.WriteLine("..AND you want pastries? Geesh. There's a wordier sale I don't want to go it to. Buy a bunch and save money.");
+      Console.WriteLine("I don't got all day. How many you want?");
+      Console.ReadLine(bStringVar);
+      Console.WriteLine("Gimme $" + "string" + ". Now git.")
     }
   }
 }
