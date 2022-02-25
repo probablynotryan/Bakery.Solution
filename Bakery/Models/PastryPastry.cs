5a847a02f371425da91e988a4a5a3131daeb2272
Ryan Bass <theryanbass@gmail.com>
2022-02-25 13:56:49 -0800

    return to prior code that clears tests

--- Bakery/Models/PastryPastry.cs
+++ Bakery/Models/PastryPastry.cs
@@ -6,7 +6,13 @@ namespace Bakery.Model
   {
     public int PastryOrder (int numOfPastries)
     {
-      return (numOfPastries + (int)Math.Ceiling(Decimal.Divide(numOfPastries, 2)) + (numOfPastries / 5));
+      int pastryCost = 0;
+      while (numOfPastries > 5)
+      {
+        pastryCost += 10;
+        numOfPastries -= 6;
+      } 
+      return pastryCost + numOfPastries + (int)Math.Ceiling(Decimal.Divide(numOfPastries, 2)) + (numOfPastries / 4);
     } 
   }
 }
