223780da09410394d5508f3c15d65f6551007060
Ryan Bass <theryanbass@gmail.com>
2022-02-25 09:35:36 -0800

    change bread method test to green

--- Bakery.Tests/ModelTests/PierresBakeryTests.cs
+++ Bakery.Tests/ModelTests/PierresBakeryTests.cs
@@ -12,12 +12,12 @@ namespace Bakery.Test
       PierresBakery testBakeryOrder = new PierresBakery();
       Assert.AreEqual("Ding ding!", testBakeryOrder.BakeryOrder(124));
     }
-    
+
     [TestMethod]
-    public void BreadOrder_OneReturnsThree_Three()
+    public void BreadOrder_1Returns5_5()
       {
         PierresBakery testBreadOrder = new PierresBakery();
-        Assert.AreEqual(3, testBreadOrder.BreadOrder(1));
+        Assert.AreEqual(5, testBreadOrder.BreadOrder(1));
       }
   }
 }
