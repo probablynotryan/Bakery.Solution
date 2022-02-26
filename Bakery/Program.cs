1623926b2f4d398179b15c73293632a53b7d0ec6
Ryan Bass <theryanbass@gmail.com>
2022-02-25 16:37:08 -0800

    stylize ui output with color

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,10 +9,24 @@ namespace Bakery
     {
       BreadBread Cat = new BreadBread();
       PastryPastry Dog = new PastryPastry();
-      Console.WriteLine("How many breads?");
+      Console.Write("Welcome to ");
+      Console.ForegroundColor = ConsoleColor.Green;
+      Console.Write("Pierre's Bakery. ");
+      Console.ResetColor();
+      Console.WriteLine("Right now, breads are buy two get one and pastries are buy three get the third one half-off. ");
+      Console.Write("How many ");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.Write("breads ");
+      Console.ResetColor();
+      Console.WriteLine("would you like? ($5/loaf)");
       string inOne = Console.ReadLine();
-      Console.WriteLine("How many pastries-es?");
+      Console.Write("How many ");
+      Console.ForegroundColor = ConsoleColor.Blue;
+      Console.Write("pastries ");
+      Console.ResetColor();
+      Console.WriteLine("would you like? ($2/pastry)");
       string inTwo = Console.ReadLine();
+      Console.ForegroundColor = ConsoleColor.Green;
       Console.WriteLine("Your total is: $" + (Dog.PastryOrder(int.Parse(inTwo)) + Cat.BreadOrder(int.Parse(inOne))));
     }
   }
