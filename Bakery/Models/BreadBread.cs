00e22187732eecf46def5d581001d170d1c8bb02
Ryan Bass <theryanbass@gmail.com>
2022-03-03 14:58:34 -0800

    change methods to not be static to allow for smoother testing

--- Bakery/Models/BreadBread.cs
+++ Bakery/Models/BreadBread.cs
@@ -4,8 +4,8 @@ namespace Bakery.Model
 {
   public class BreadBread 
   {
-    public static int Breads { get; set; }
-    public static int BreadOrder(int numOfBreads)
+    public int Breads { get; set; }
+    public int BreadOrder(int numOfBreads)
     {
       Breads = numOfBreads;
       return ((Breads - (Breads / 3)) * 5);
--- Bakery/Models/PastryPastry.cs
+++ Bakery/Models/PastryPastry.cs
@@ -4,8 +4,8 @@ namespace Bakery.Model
 {
   public class PastryPastry
   {
-    public static int NumberOfPastries { get; set; }
-    public static int PastryOrder (int pastries)
+    public int NumberOfPastries { get; set; }
+    public int PastryOrder (int pastries)
     {
       NumberOfPastries = pastries;
       return ((NumberOfPastries / 3) * 5) + ((NumberOfPastries % 3) * 2);
